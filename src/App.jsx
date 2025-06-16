5fc7b84abb0732ff2bc68fa2053b32123fb853c2
PranaySAG <pranayshinde929@gmail.com>
2025-06-16 21:52:06 +0530

    feat: Integrate Job page components and routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import {
 import Sidebar, { SidebarItem } from "./components/UIElments/Sidebar";
 import GeminiImageText from "./components/models/main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import Job from "./pages/sidebar/Jobs";
 import Projects from "./pages/sidebar/Projects";
 import HomePage from "./pages/sidebar/HomePage";
 
@@ -24,7 +24,7 @@ function App() {
           <SidebarItem icon={<Home size={20} />} text="Home" to="/" />
           <SidebarItem icon={<LayoutDashboard size={20} />} text="AI" to="/ai" />
           <SidebarItem icon={<StickyNote size={20} />} text="Projects" to="/projects" />
-          <SidebarItem icon={<Calendar size={20} />} text="Calendar" to="/calendar" />
+          <SidebarItem icon={<Calendar size={20} />} text="Job" to="/job" />
           <SidebarItem icon={<Layers size={20} />} text="Tasks" to="/tasks" />
           <SidebarItem icon={<Flag size={20} />} text="Reporting" to="/reporting" />
           <hr className="my-3" />
@@ -32,7 +32,6 @@ function App() {
           <SidebarItem icon={<LifeBuoy size={20} />} text="Help" to="/help" />
         </Sidebar>
 
-        {/* Main Content */}
         <div className="flex-1 flex flex-col">
         
           <div className="flex-1">
@@ -40,7 +39,7 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/ai" element={<GeminiImageText />} />
               <Route path="/projects" element={<Projects />} />
-              <Route path="/calendar" element={<h1>Calendar Page</h1>} />
+              <Route path="/job" element={<Job/ >} />
               <Route path="/tasks" element={<h1>Tasks Page</h1>} />
               <Route path="/reporting" element={<h1>Reporting Page</h1>} />
               <Route path="/settings" element={<h1>Settings Page</h1>} />
